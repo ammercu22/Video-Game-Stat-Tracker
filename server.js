1fee11a937d2629195a9df9ffda287a1b77925ce
Alliyah <amercur@siue.edu>
2021-12-26 11:06:51 -0600

    make it better

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const port = process.env.PORT || 8000;
 //Handle Production
 if(process.env.NODE_ENV==='production'){
     //Set static folder
-    app.use(express.static(__dirname + '/client/build/'));
+    app.use(express.static(__dirname + '/build/'));
 
     //Handle SPA
-    app.get(/.*/, (req,res) => res.sendFile(__dirname + 'client/build/index.html'));
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/build/index.html'));
 }
 app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
