c96c4ad95bbe5523490746b77755761b244d3cbd
Alliyah <amercur@siue.edu>
2021-12-26 11:47:10 -0600

    updates

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ if(process.env.NODE_ENV==='production'){
     app.get(/.*/, (req,res) => res.sendFile(__dirname + '/build/index.html'));
 }
 app.listen(port, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}` )
 });
