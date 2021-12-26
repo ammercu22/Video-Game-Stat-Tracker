20890915c359c131fbe24f61b97e454ba72676bb
Alliyah <amercur@siue.edu>
2021-12-26 11:36:05 -0600

    make it better

---
+++ build/precache-manifest.c4848621a3324086ad0accae6e97f4b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97183099819dbddf9e1ba729b23055cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce5255a0a9a329a5d8d",
+    "url": "/static/css/main.77c55c36.chunk.css"
+  },
+  {
+    "revision": "7232692b31868d9177ae",
+    "url": "/static/js/2.86c581b6.chunk.js"
+  },
+  {
+    "revision": "4b7448a9c505669f244a7eac984d25a6",
+    "url": "/static/js/2.86c581b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce5255a0a9a329a5d8d",
+    "url": "/static/js/main.b7b3aff0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "badc059db553d3c4d014ad48534b9567",
+    "url": "/static/media/ApexLegendsLogo.badc059d.png"
+  },
+  {
+    "revision": "0806c58d402f7f9e512c1ddbaca4ffc1",
+    "url": "/static/media/octane.0806c58d.png"
+  }
+]);
